a7fae700260a257c354cab28242626bad0e78cf9
Burcu Dogan <burcujdogan@gmail.com>
2014-02-16 21:17:45 +0100

    Add docs.

--- fs/fs.go
+++ fs/fs.go
@@ -1,3 +1,18 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package contains an HTTP file system that works with zip contents.
 package fs
 
 import (
@@ -18,10 +33,13 @@ type statikFS struct {
 	files map[string]*zip.File
 }
 
+// Registers zip contents data, later used to initialize
+// the statik file system.
 func Register(data string) {
 	zipData = data
 }
 
+// Creates a new file system with the registered zip contents data.
 func New() (http.FileSystem, error) {
 	if zipData == "" {
 		return nil, errors.New("statik/fs: No zip data registered.")
@@ -39,6 +57,9 @@ func New() (http.FileSystem, error) {
 	return &statikFS{files: files}, nil
 }
 
+// Opens a file, unzip the contents and initializes
+// readers. Returns os.ErrNotExists if file is not
+// found in the archive.
 func (fs *statikFS) Open(name string) (http.File, error) {
 	f, ok := fs.files[name]
 	if !ok {
@@ -68,6 +89,8 @@ func newFile(zf *zip.File) (*file, error) {
 	}, nil
 }
 
+// Represents an HTTP file, acts as a bridge between
+// zip.File and http.File.
 type file struct {
 	os.FileInfo
 	io.Closer
@@ -83,20 +106,25 @@ func (f *file) newReader() {
 	f.reader = io.NewSectionReader(f.readerAt, 0, f.FileInfo.Size())
 }
 
+// Reads bytes into p, returns the number of read bytes.
 func (f *file) Read(p []byte) (n int, err error) {
 	f.once.Do(f.newReader)
 	return f.reader.Read(p)
 }
 
+// Seeks to the offset.
 func (f *file) Seek(offset int64, whence int) (ret int64, err error) {
 	f.once.Do(f.newReader)
 	return f.reader.Seek(offset, whence)
 }
 
+// Returns FileInfo.
 func (f *file) Stat() (os.FileInfo, error) {
 	return f, nil
 }
 
+// Returns an empty slice of files, directory
+// listing is disabled.
 func (f *file) Readdir(count int) ([]os.FileInfo, error) {
 	// directory listing is disabled.
 	return make([]os.FileInfo, 0), nil
