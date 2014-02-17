980d3183b06593ac96b637ad8546a98eeadda2ef
Burcu Dogan <burcujdogan@gmail.com>
2014-02-17 13:14:20 +0100

    Adding an example program.

---
+++ example/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	_ "github.com/rakyll/statik/example/statik"
+	"github.com/rakyll/statik/fs"
+)
+
+// Before running, run `statik -src=./public`
+// to regenerate the statik package.
+// Then, run the main program and visit http://localhost:8080/hello.txt
+func main() {
+	statikFS, err := fs.New()
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+
+	http.ListenAndServe(":8080", http.FileServer(statikFS))
+}
