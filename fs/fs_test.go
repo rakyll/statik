c6d0b13a5b1edaa2220fa3a1b678041ecf33dd8e
Matthew Dale <matthew@matthewrdale.com>
2017-04-08 21:18:05 -0700

    use Go idioms for 'fs' package test

--- fs/fs_test.go
+++ fs/fs_test.go
@@ -26,7 +26,7 @@ import (
 	"time"
 )
 
-type expectedFile struct {
+type wantFile struct {
 	data    []byte
 	isDir   bool
 	modTime time.Time
@@ -37,81 +37,85 @@ type expectedFile struct {
 }
 
 func TestOpen(t *testing.T) {
+	fileTxtHeader := mustFileHeader("../testdata/file/file.txt")
+	pixelGifHeader := mustFileHeader("../testdata/image/pixel.gif")
+	indexHTMLHeader := mustFileHeader("../testdata/index/index.html")
+	subdirIndexHTMLHeader := mustFileHeader("../testdata/index/sub_dir/index.html")
 	tests := []struct {
-		description   string
-		zipData       string
-		expectedFiles map[string]expectedFile
+		description string
+		zipData     string
+		wantFiles   map[string]wantFile
 	}{
 		{
 			description: "Files should retain their original file mode and modified time",
 			zipData:     mustZipTree("../testdata/file"),
-			expectedFiles: map[string]expectedFile{
+			wantFiles: map[string]wantFile{
 				"/file.txt": {
 					data:    mustReadFile("../testdata/file/file.txt"),
 					isDir:   false,
-					modTime: mustStatFile("../testdata/file/file.txt").ModTime(),
-					mode:    mustStatFile("../testdata/file/file.txt").Mode(),
-					name:    mustStatFile("../testdata/file/file.txt").Name,
-					size:    int64(mustStatFile("../testdata/file/file.txt").UncompressedSize64),
+					modTime: fileTxtHeader.ModTime(),
+					mode:    fileTxtHeader.Mode(),
+					name:    fileTxtHeader.Name,
+					size:    int64(fileTxtHeader.UncompressedSize64),
 				},
 			},
 		},
 		{
 			description: "Images should successfully unpack",
 			zipData:     mustZipTree("../testdata/image"),
-			expectedFiles: map[string]expectedFile{
+			wantFiles: map[string]wantFile{
 				"/pixel.gif": {
 					data:    mustReadFile("../testdata/image/pixel.gif"),
 					isDir:   false,
-					modTime: mustStatFile("../testdata/image/pixel.gif").ModTime(),
-					mode:    mustStatFile("../testdata/image/pixel.gif").Mode(),
-					name:    mustStatFile("../testdata/image/pixel.gif").Name,
-					size:    int64(mustStatFile("../testdata/image/pixel.gif").UncompressedSize64),
+					modTime: pixelGifHeader.ModTime(),
+					mode:    pixelGifHeader.Mode(),
+					name:    pixelGifHeader.Name,
+					size:    int64(pixelGifHeader.UncompressedSize64),
 				},
 			},
 		},
 		{
 			description: "'index.html' files should be returned at their original path and their directory path",
 			zipData:     mustZipTree("../testdata/index"),
-			expectedFiles: map[string]expectedFile{
+			wantFiles: map[string]wantFile{
 				"/index.html": {
 					data:    mustReadFile("../testdata/index/index.html"),
 					isDir:   false,
-					modTime: mustStatFile("../testdata/index/index.html").ModTime(),
-					mode:    mustStatFile("../testdata/index/index.html").Mode(),
-					name:    mustStatFile("../testdata/index/index.html").Name,
-					size:    int64(mustStatFile("../testdata/index/index.html").UncompressedSize64),
+					modTime: indexHTMLHeader.ModTime(),
+					mode:    indexHTMLHeader.Mode(),
+					name:    indexHTMLHeader.Name,
+					size:    int64(indexHTMLHeader.UncompressedSize64),
 				},
 				"/": {
 					data:    mustReadFile("../testdata/index/index.html"),
 					isDir:   true,
-					modTime: mustStatFile("../testdata/index/index.html").ModTime(),
-					mode:    mustStatFile("../testdata/index/index.html").Mode(),
-					name:    mustStatFile("../testdata/index/index.html").Name,
-					size:    int64(mustStatFile("../testdata/index/index.html").UncompressedSize64),
+					modTime: indexHTMLHeader.ModTime(),
+					mode:    indexHTMLHeader.Mode(),
+					name:    indexHTMLHeader.Name,
+					size:    int64(indexHTMLHeader.UncompressedSize64),
 				},
 				"/sub_dir/index.html": {
 					data:    mustReadFile("../testdata/index/sub_dir/index.html"),
 					isDir:   false,
-					modTime: mustStatFile("../testdata/index/sub_dir/index.html").ModTime(),
-					mode:    mustStatFile("../testdata/index/sub_dir/index.html").Mode(),
-					name:    mustStatFile("../testdata/index/sub_dir/index.html").Name,
-					size:    int64(mustStatFile("../testdata/index/sub_dir/index.html").UncompressedSize64),
+					modTime: subdirIndexHTMLHeader.ModTime(),
+					mode:    subdirIndexHTMLHeader.Mode(),
+					name:    subdirIndexHTMLHeader.Name,
+					size:    int64(subdirIndexHTMLHeader.UncompressedSize64),
 				},
 				"/sub_dir/": {
 					data:    mustReadFile("../testdata/index/sub_dir/index.html"),
 					isDir:   true,
-					modTime: mustStatFile("../testdata/index/sub_dir/index.html").ModTime(),
-					mode:    mustStatFile("../testdata/index/sub_dir/index.html").Mode(),
-					name:    mustStatFile("../testdata/index/sub_dir/index.html").Name,
-					size:    int64(mustStatFile("../testdata/index/sub_dir/index.html").UncompressedSize64),
+					modTime: subdirIndexHTMLHeader.ModTime(),
+					mode:    subdirIndexHTMLHeader.Mode(),
+					name:    subdirIndexHTMLHeader.Name,
+					size:    int64(subdirIndexHTMLHeader.UncompressedSize64),
 				},
 			},
 		},
 		{
 			description: "Missing files should return os.ErrNotExist",
 			zipData:     mustZipTree("../testdata/file"),
-			expectedFiles: map[string]expectedFile{
+			wantFiles: map[string]wantFile{
 				"/missing.txt": {
 					err: os.ErrNotExist,
 				},
@@ -123,71 +127,43 @@ func TestOpen(t *testing.T) {
 			Register(tc.zipData)
 			fs, err := New()
 			if err != nil {
-				t.Errorf("Error creating new fs: %s", err)
+				t.Errorf("New() = %v", err)
 				return
 			}
-			for name, expectedFile := range tc.expectedFiles {
+			for name, wantFile := range tc.wantFiles {
 				f, err := fs.Open(name)
-				if expectedFile.err != err {
-					t.Errorf(
-						"Expected and actual error opening file different for file %q.\nExpected:\t%s\nActual:\t\t%s",
-						name,
-						expectedFile.err,
-						err)
+				if wantFile.err != err {
+					t.Errorf("fs.Open(%v) = %v; want %v", name, err, wantFile.err)
 				}
 				if err != nil {
 					continue
 				}
 				b, err := ioutil.ReadAll(f)
 				if err != nil {
-					t.Errorf(
-						"Error reading file %q: %s", name, err)
+					t.Errorf("ioutil.ReadAll(%v) = %v", name, err)
 					continue
 				}
-				if !reflect.DeepEqual(expectedFile.data, b) {
-					t.Errorf(
-						"Expected and actual file data different for file %q.\nExpected:\t%v\nActual:\t\t%v",
-						name,
-						expectedFile.data,
-						b)
+				if !reflect.DeepEqual(wantFile.data, b) {
+					t.Errorf("%v data = %q; want %q", name, b, wantFile.data)
 				}
-				stat, _ := f.Stat()
-				if expectedFile.isDir != stat.IsDir() {
-					t.Errorf(
-						"Expected and actual file IsDir different for file %q.\nExpected:\t%t\nActual:\t\t%t",
-						name,
-						expectedFile.isDir,
-						stat.IsDir())
+				stat, err := f.Stat()
+				if err != nil {
+					t.Errorf("Stat(%v) = %v", name, err)
 				}
-				if expectedFile.modTime != stat.ModTime() {
-					t.Errorf(
-						"Expected and actual file ModTime different for file %q.\nExpected:\t%s (%d)\nActual:\t\t%s (%d)",
-						name,
-						expectedFile.modTime,
-						expectedFile.modTime.UnixNano(),
-						stat.ModTime(),
-						stat.ModTime().UnixNano())
+				if got, want := stat.IsDir(), wantFile.isDir; got != want {
+					t.Errorf("IsDir(%v) = %t; want %t", name, got, want)
 				}
-				if expectedFile.mode != stat.Mode() {
-					t.Errorf(
-						"Expected and actual file Mode different for file %q.\nExpected:\t%s\nActual:\t\t%s",
-						name,
-						expectedFile.mode,
-						stat.Mode())
+				if got, want := stat.ModTime(), wantFile.modTime; got != want {
+					t.Errorf("ModTime(%v) = %v; want %v", name, got, want)
 				}
-				if expectedFile.name != stat.Name() {
-					t.Errorf(
-						"Expected and actual file Name different for file %q.\nExpected:\t%s\nActual:\t\t%s",
-						name,
-						expectedFile.name,
-						stat.Name())
+				if got, want := stat.Mode(), wantFile.mode; got != want {
+					t.Errorf("Mode(%v) = %v; want %v", name, got, want)
 				}
-				if expectedFile.size != stat.Size() {
-					t.Errorf(
-						"Expected and actual file Size different for file %q.\nExpected:\t%d\nActual:\t\t%d",
-						name,
-						expectedFile.size,
-						stat.Size())
+				if got, want := stat.Name(), wantFile.name; got != want {
+					t.Errorf("Name(%v) = %v; want %v", name, got, want)
+				}
+				if got, want := stat.Size(), wantFile.size; got != want {
+					t.Errorf("Size(%v) = %v; want %v", name, got, want)
 				}
 			}
 		})
@@ -197,32 +173,30 @@ func TestOpen(t *testing.T) {
 // Test that calling Open by many goroutines concurrently continues
 // to return the expected result.
 func TestOpen_Parallel(t *testing.T) {
-	indexFileContents := mustReadFile("../testdata/index/index.html")
+	indexHTMLData := mustReadFile("../testdata/index/index.html")
 	Register(mustZipTree("../testdata/index"))
 	fs, err := New()
 	if err != nil {
-		t.Fatalf("Error creating new fs: %s", err)
+		t.Fatalf("New() = %v", err)
 	}
 	wg := sync.WaitGroup{}
 	for i := 0; i < 128; i++ {
 		wg.Add(1)
 		go func() {
 			defer wg.Done()
-			f, err := fs.Open("/index.html")
+			name := "/index.html"
+			f, err := fs.Open(name)
 			if err != nil {
-				t.Errorf("Error opening file '/index.html': %s", err)
+				t.Errorf("fs.Open(%v) = %v", name, err)
 				return
 			}
 			b, err := ioutil.ReadAll(f)
 			if err != nil {
-				t.Errorf("Error reading file '/index.html': %s", err)
+				t.Errorf("ioutil.ReadAll(%v) = %v", name, err)
 				return
 			}
-			if !reflect.DeepEqual(indexFileContents, b) {
-				t.Errorf(
-					"Expected and actual file data different for file '/index.html'.\nExpected:\t%v\nActual:\t\t%v",
-					indexFileContents,
-					b)
+			if !reflect.DeepEqual(indexHTMLData, b) {
+				t.Errorf("%v data = %q; want %q", name, b, indexHTMLData)
 			}
 		}()
 	}
@@ -233,13 +207,14 @@ func BenchmarkOpen(b *testing.B) {
 	Register(mustZipTree("../testdata/index"))
 	fs, err := New()
 	if err != nil {
-		b.Fatalf("Error creating new fs: %s", err)
+		b.Fatalf("New() = %v", err)
 	}
 	b.RunParallel(func(pb *testing.PB) {
 		for pb.Next() {
-			_, err := fs.Open("/index.html")
+			name := "/index.html"
+			_, err := fs.Open(name)
 			if err != nil {
-				b.Errorf("Error opening file '/index.html': %s", err)
+				b.Errorf("fs.Open(%v) = %v", name, err)
 			}
 		}
 	})
@@ -249,16 +224,8 @@ func BenchmarkOpen(b *testing.B) {
 // as a string. Panics on any errors.
 func mustZipTree(srcPath string) string {
 	var out bytes.Buffer
-	absPath, err := filepath.Abs(srcPath)
-	if err != nil {
-		panic(err)
-	}
-	absPathWithoutSymlinks, err := filepath.EvalSymlinks(absPath)
-	if err != nil {
-		panic(err)
-	}
 	w := zip.NewWriter(&out)
-	if err := filepath.Walk(absPathWithoutSymlinks, func(path string, fi os.FileInfo, err error) error {
+	if err := filepath.Walk(srcPath, func(path string, fi os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
@@ -269,7 +236,7 @@ func mustZipTree(srcPath string) string {
 		if fi.IsDir() || strings.HasPrefix(fi.Name(), ".") {
 			return nil
 		}
-		relPath, err := filepath.Rel(absPathWithoutSymlinks, path)
+		relPath, err := filepath.Rel(srcPath, path)
 		if err != nil {
 			return err
 		}
@@ -300,30 +267,22 @@ func mustZipTree(srcPath string) string {
 
 // mustReadFile returns the file contents. Panics on any errors.
 func mustReadFile(filename string) []byte {
-	absPath, err := filepath.Abs(filename)
-	if err != nil {
-		panic(err)
-	}
-	absPathWithoutSymlinks, err := filepath.EvalSymlinks(absPath)
-	if err != nil {
-		panic(err)
-	}
-	b, err := ioutil.ReadFile(absPathWithoutSymlinks)
+	b, err := ioutil.ReadFile(filename)
 	if err != nil {
 		panic(err)
 	}
 	return b
 }
 
-// mustStatFile returns the zip file info header. Panics on any errors.
-func mustStatFile(filename string) *zip.FileHeader {
+// mustFileHeader returns the zip file info header. Panics on any errors.
+func mustFileHeader(filename string) *zip.FileHeader {
 	info, err := os.Stat(filename)
 	if err != nil {
 		panic(err)
 	}
-	zipInfo, err := zip.FileInfoHeader(info)
+	header, err := zip.FileInfoHeader(info)
 	if err != nil {
 		panic(err)
 	}
-	return zipInfo
+	return header
 }
