0a80f275bc01b6b9f5f1d204cc1bc05ce7b43e38
Matthew Dale <matthew@matthewrdale.com>
2018-08-02 17:39:40 -0700

    add tests and benchmarks for the 'fs' package (#22)

---
+++ fs/fs_test.go
@@ -0,0 +1,288 @@
+// Copyright 2014 Google Inc. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package fs
+
+import (
+	"archive/zip"
+	"bytes"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strings"
+	"sync"
+	"testing"
+	"time"
+)
+
+type wantFile struct {
+	data    []byte
+	isDir   bool
+	modTime time.Time
+	mode    os.FileMode
+	name    string
+	size    int64
+	err     error
+}
+
+func TestOpen(t *testing.T) {
+	fileTxtHeader := mustFileHeader("../testdata/file/file.txt")
+	pixelGifHeader := mustFileHeader("../testdata/image/pixel.gif")
+	indexHTMLHeader := mustFileHeader("../testdata/index/index.html")
+	subdirIndexHTMLHeader := mustFileHeader("../testdata/index/sub_dir/index.html")
+	tests := []struct {
+		description string
+		zipData     string
+		wantFiles   map[string]wantFile
+	}{
+		{
+			description: "Files should retain their original file mode and modified time",
+			zipData:     mustZipTree("../testdata/file"),
+			wantFiles: map[string]wantFile{
+				"/file.txt": {
+					data:    mustReadFile("../testdata/file/file.txt"),
+					isDir:   false,
+					modTime: fileTxtHeader.ModTime(),
+					mode:    fileTxtHeader.Mode(),
+					name:    fileTxtHeader.Name,
+					size:    int64(fileTxtHeader.UncompressedSize64),
+				},
+			},
+		},
+		{
+			description: "Images should successfully unpack",
+			zipData:     mustZipTree("../testdata/image"),
+			wantFiles: map[string]wantFile{
+				"/pixel.gif": {
+					data:    mustReadFile("../testdata/image/pixel.gif"),
+					isDir:   false,
+					modTime: pixelGifHeader.ModTime(),
+					mode:    pixelGifHeader.Mode(),
+					name:    pixelGifHeader.Name,
+					size:    int64(pixelGifHeader.UncompressedSize64),
+				},
+			},
+		},
+		{
+			description: "'index.html' files should be returned at their original path and their directory path",
+			zipData:     mustZipTree("../testdata/index"),
+			wantFiles: map[string]wantFile{
+				"/index.html": {
+					data:    mustReadFile("../testdata/index/index.html"),
+					isDir:   false,
+					modTime: indexHTMLHeader.ModTime(),
+					mode:    indexHTMLHeader.Mode(),
+					name:    indexHTMLHeader.Name,
+					size:    int64(indexHTMLHeader.UncompressedSize64),
+				},
+				"/": {
+					data:    mustReadFile("../testdata/index/index.html"),
+					isDir:   true,
+					modTime: indexHTMLHeader.ModTime(),
+					mode:    indexHTMLHeader.Mode(),
+					name:    indexHTMLHeader.Name,
+					size:    int64(indexHTMLHeader.UncompressedSize64),
+				},
+				"/sub_dir/index.html": {
+					data:    mustReadFile("../testdata/index/sub_dir/index.html"),
+					isDir:   false,
+					modTime: subdirIndexHTMLHeader.ModTime(),
+					mode:    subdirIndexHTMLHeader.Mode(),
+					name:    subdirIndexHTMLHeader.Name,
+					size:    int64(subdirIndexHTMLHeader.UncompressedSize64),
+				},
+				"/sub_dir/": {
+					data:    mustReadFile("../testdata/index/sub_dir/index.html"),
+					isDir:   true,
+					modTime: subdirIndexHTMLHeader.ModTime(),
+					mode:    subdirIndexHTMLHeader.Mode(),
+					name:    subdirIndexHTMLHeader.Name,
+					size:    int64(subdirIndexHTMLHeader.UncompressedSize64),
+				},
+			},
+		},
+		{
+			description: "Missing files should return os.ErrNotExist",
+			zipData:     mustZipTree("../testdata/file"),
+			wantFiles: map[string]wantFile{
+				"/missing.txt": {
+					err: os.ErrNotExist,
+				},
+			},
+		},
+	}
+	for _, tc := range tests {
+		t.Run(tc.description, func(t *testing.T) {
+			Register(tc.zipData)
+			fs, err := New()
+			if err != nil {
+				t.Errorf("New() = %v", err)
+				return
+			}
+			for name, wantFile := range tc.wantFiles {
+				f, err := fs.Open(name)
+				if wantFile.err != err {
+					t.Errorf("fs.Open(%v) = %v; want %v", name, err, wantFile.err)
+				}
+				if err != nil {
+					continue
+				}
+				b, err := ioutil.ReadAll(f)
+				if err != nil {
+					t.Errorf("ioutil.ReadAll(%v) = %v", name, err)
+					continue
+				}
+				if !reflect.DeepEqual(wantFile.data, b) {
+					t.Errorf("%v data = %q; want %q", name, b, wantFile.data)
+				}
+				stat, err := f.Stat()
+				if err != nil {
+					t.Errorf("Stat(%v) = %v", name, err)
+				}
+				if got, want := stat.IsDir(), wantFile.isDir; got != want {
+					t.Errorf("IsDir(%v) = %t; want %t", name, got, want)
+				}
+				if got, want := stat.ModTime(), wantFile.modTime; got != want {
+					t.Errorf("ModTime(%v) = %v; want %v", name, got, want)
+				}
+				if got, want := stat.Mode(), wantFile.mode; got != want {
+					t.Errorf("Mode(%v) = %v; want %v", name, got, want)
+				}
+				if got, want := stat.Name(), wantFile.name; got != want {
+					t.Errorf("Name(%v) = %v; want %v", name, got, want)
+				}
+				if got, want := stat.Size(), wantFile.size; got != want {
+					t.Errorf("Size(%v) = %v; want %v", name, got, want)
+				}
+			}
+		})
+	}
+}
+
+// Test that calling Open by many goroutines concurrently continues
+// to return the expected result.
+func TestOpen_Parallel(t *testing.T) {
+	indexHTMLData := mustReadFile("../testdata/index/index.html")
+	Register(mustZipTree("../testdata/index"))
+	fs, err := New()
+	if err != nil {
+		t.Fatalf("New() = %v", err)
+	}
+	wg := sync.WaitGroup{}
+	for i := 0; i < 128; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			name := "/index.html"
+			f, err := fs.Open(name)
+			if err != nil {
+				t.Errorf("fs.Open(%v) = %v", name, err)
+				return
+			}
+			b, err := ioutil.ReadAll(f)
+			if err != nil {
+				t.Errorf("ioutil.ReadAll(%v) = %v", name, err)
+				return
+			}
+			if !reflect.DeepEqual(indexHTMLData, b) {
+				t.Errorf("%v data = %q; want %q", name, b, indexHTMLData)
+			}
+		}()
+	}
+	wg.Wait()
+}
+
+func BenchmarkOpen(b *testing.B) {
+	Register(mustZipTree("../testdata/index"))
+	fs, err := New()
+	if err != nil {
+		b.Fatalf("New() = %v", err)
+	}
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			name := "/index.html"
+			_, err := fs.Open(name)
+			if err != nil {
+				b.Errorf("fs.Open(%v) = %v", name, err)
+			}
+		}
+	})
+}
+
+// mustZipTree walks on the source path and returns the zipped file contents
+// as a string. Panics on any errors.
+func mustZipTree(srcPath string) string {
+	var out bytes.Buffer
+	w := zip.NewWriter(&out)
+	if err := filepath.Walk(srcPath, func(path string, fi os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		// Ignore directories and hidden files.
+		// No entry is needed for directories in a zip file.
+		// Each file is represented with a path, no directory
+		// entities are required to build the hierarchy.
+		if fi.IsDir() || strings.HasPrefix(fi.Name(), ".") {
+			return nil
+		}
+		relPath, err := filepath.Rel(srcPath, path)
+		if err != nil {
+			return err
+		}
+		b, err := ioutil.ReadFile(path)
+		if err != nil {
+			return err
+		}
+		fHeader, err := zip.FileInfoHeader(fi)
+		if err != nil {
+			return err
+		}
+		fHeader.Name = filepath.ToSlash(relPath)
+		fHeader.Method = zip.Deflate
+		f, err := w.CreateHeader(fHeader)
+		if err != nil {
+			return err
+		}
+		_, err = f.Write(b)
+		return err
+	}); err != nil {
+		panic(err)
+	}
+	if err := w.Close(); err != nil {
+		panic(err)
+	}
+	return string(out.Bytes())
+}
+
+// mustReadFile returns the file contents. Panics on any errors.
+func mustReadFile(filename string) []byte {
+	b, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	return b
+}
+
+// mustFileHeader returns the zip file info header. Panics on any errors.
+func mustFileHeader(filename string) *zip.FileHeader {
+	info, err := os.Stat(filename)
+	if err != nil {
+		panic(err)
+	}
+	header, err := zip.FileInfoHeader(info)
+	if err != nil {
+		panic(err)
+	}
+	return header
+}
